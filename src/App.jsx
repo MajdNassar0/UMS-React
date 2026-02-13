73d50b835d71af2ba7626b0869bfd7d3019dc5c4
MajdNassar0 <majdmnassar05@gmail.com>
2026-02-13 23:16:07 +0200

    add spa

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import Navbar from './components/navbar/Navbar'
+import Footer from './components/footer/Footer'
+import { Route, Routes } from 'react-router-dom'
+import Home from './page/home/Home'
+import User from './page/user/User'
+import Register from './page/register/Register'
 
 function App() {
   return (
-    <div>
-      <div className="text-danger">app</div>
-    </div>
+    <>
+    <Navbar />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/user' element={<User />} />
+      <Route path='/register' element={<Register />} />
+    </Routes>
+    <Footer />
+    </>
   )
 }
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Navbar() {
   return (
-    <div>
-      
+   <>
+  <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Navbar</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon" />
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div className="navbar-nav">
+        <Link className="nav-link active" aria-current="page" to={'/'}>Home</Link>
+        <Link className="nav-link" to={'/user'}>Users</Link>
+        <Link className="nav-link" to={'/register'}>Register</Link>
+      </div>
     </div>
+  </div>
+</nav>
+
+   </>
   )
 }
 
